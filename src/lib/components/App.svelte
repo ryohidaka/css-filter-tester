6362ab292f7eb5ac4df084fb6a0586694c7cfde0
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-09 00:37:23 +0900

    feat: Add PreviewTable.svelte.

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,7 +1,7 @@
 <!-- This is a Svelte component -->
 <script lang="ts">
 	import ImageSelector from './ImageSelector.svelte';
-	import Preview from './Preview.svelte';
+	import PreviewTable from './PreviewTable.svelte';
 </script>
 
 <h1>CSS Filter Test</h1>
@@ -12,23 +12,7 @@
 </div>
 
 <!-- Preview -->
-<table id="preview">
-	<thead>
-		<tr>
-			<th>Original</th>
-			<th>Filtered</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<!-- Original Image -->
-			<td><Preview /></td>
-
-			<!-- Filtered Image -->
-			<td><Preview /></td>
-		</tr>
-	</tbody>
-</table>
+<PreviewTable />
 
 <!-- You can add inline CSS styles here to style the component -->
 <style>
---
+++ src/lib/components/PreviewTable.svelte
@@ -0,0 +1,23 @@
+<script>
+	// Import Preview component
+	import Preview from './Preview.svelte';
+</script>
+
+<!-- Create a table for displaying original and filtered images -->
+<table id="preview-table">
+	<thead>
+		<tr>
+			<th>Original</th>
+			<th>Filtered</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<!-- Original Image Preview -->
+			<td><Preview /></td>
+
+			<!-- Filtered Image Preview -->
+			<td><Preview /></td>
+		</tr>
+	</tbody>
+</table>

000fbbf991e666e725f4250b0012a8c84c995672
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-08 22:52:33 +0900

    feat: Add App.svelte.

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,14 @@
+<!-- This is a Svelte component -->
+<script lang="ts">
+	// You can add TypeScript code here to define the component's behavior.
+</script>
+
+<!-- The following is the HTML markup for the component -->
+<h1>CSS Filter Test</h1>
+
+<!-- The above code represents a simple Svelte component with HTML, TypeScript, and CSS sections. -->
+
+<!-- You can add inline CSS styles here to style the component -->
+<style>
+	/* Add CSS styles to customize the appearance of the component */
+</style>

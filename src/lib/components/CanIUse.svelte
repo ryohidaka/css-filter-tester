1ffe4fda50fceeb63ac41a0432c3cbd9a22fdba2
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-10 00:30:48 +0900

    feat: Add Can I Use link.

--- src/lib/components/CanIUse.svelte
+++ src/lib/components/CanIUse.svelte
@@ -1,12 +1,15 @@
 <section id="can-i-use">
 	<h2>Can I Use</h2>
+	<div>
+		<picture>
+			<source type="image/webp" srcset="https://caniuse.bitsofco.de/image/css-filters.webp" />
+			<source type="image/png" srcset="https://caniuse.bitsofco.de/image/css-filters.png" />
+			<img
+				src="https://caniuse.bitsofco.de/image/css-filters.jpg"
+				alt="Data on support for the css-filters feature across the major browsers from caniuse.com"
+			/>
+		</picture>
+	</div>
 
-	<picture>
-		<source type="image/webp" srcset="https://caniuse.bitsofco.de/image/css-filters.webp" />
-		<source type="image/png" srcset="https://caniuse.bitsofco.de/image/css-filters.png" />
-		<img
-			src="https://caniuse.bitsofco.de/image/css-filters.jpg"
-			alt="Data on support for the css-filters feature across the major browsers from caniuse.com"
-		/>
-	</picture>
+	<a href="https://caniuse.com/css-filters">https://caniuse.com/css-filters</a>
 </section>

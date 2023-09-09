7c9ecf301a676d5bfa9302881fc55a62dbd436bf
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-10 00:27:53 +0900

    feat: Add header.

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	// Import necessary constants and components.
-	import { APPLICATION_TITLE } from '$constants/config';
 	import { FILTERS } from '$constants/filter';
 	import CanIUse from './CanIUse.svelte';
 	import Container from './Container.svelte';
@@ -8,13 +7,6 @@
 	import FilterSelector from './FilterSelector.svelte';
 	import ImageSelector from './ImageSelector.svelte';
 </script>
-
-<svelte:head>
-	<title>{APPLICATION_TITLE}</title>
-	<html lang="en" />
-</svelte:head>
-
-<h1>{APPLICATION_TITLE}</h1>
 
 <!-- Filter Selector -->
 <FilterSelector />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import App from '$components/App.svelte';
 	import Footer from '$components/Footer.svelte';
+	import Header from '$components/Header.svelte';
 </script>
 
+<Header />
 <App />
 <Footer />

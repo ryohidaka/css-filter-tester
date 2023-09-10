6aa43ba068ba7b2f7fd24e297860b27044a2e5ad
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-11 07:53:56 +0900

    feat: Change code block to skelton.

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	// Import statements
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+	// Variable for filtering
 	export let filterString = '';
+
+	// Store the highlight.js configuration
+	storeHighlightJs.set(hljs);
 </script>
 
-<pre id="code">
-	<code>
-		<!-- Displaying the computed filter string -->
-		{filterString}
-	</code>
-</pre>
+<div id="code" data-theme="skeleton" class="w-full py-3">
+	<!-- Display the computed filter string -->
+	<CodeBlock language="css" code={filterString} />
+</div>

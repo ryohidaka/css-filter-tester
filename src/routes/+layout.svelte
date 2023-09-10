58b634d2e52ffe06234536cbd43b2a99c8c85946
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-10 12:33:37 +0900

    feat: INstall tailwind.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Footer from '$components/Footer.svelte';
+	import Header from '$components/Header.svelte';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />

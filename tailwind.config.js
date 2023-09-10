264a01a25fb22c0e6fd3fb89394a34b626b55f38
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-11 07:52:52 +0900

    feat: Install skelton.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import { join } from 'path';
+// Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+		skeleton({
+			themes: { preset: ['skeleton'] }
+		})
+	]
 };
